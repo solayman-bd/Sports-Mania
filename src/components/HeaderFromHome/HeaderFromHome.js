93f19a2f552ca06f152d324e86e1889ef6089c88
Md. Solayman <mdsolaymannuhin@gmail.com>
2021-03-09 23:52:13 +0600

    first-commmit

---
+++ src/components/HeaderFromHome/HeaderFromHome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeaderFromHome = () => {
+  return (
+    <div>
+      <h1>I am Header From Home</h1>
+    </div>
+  );
+};
+
+export default HeaderFromHome;
---
+++ src/components/LeagueDetails/LeagueDetails.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LeagueDetails = () => {
+  return (
+    <div>
+      <h1>I am from League Details</h1>
+    </div>
+  );
+};
+
+export default LeagueDetails;

e1c7d7e09a25f9b81a6be8ea18a595e1f43d399d
Md. Solayman <mdsolaymannuhin@gmail.com>
2021-03-11 11:43:51 +0600

    dynamicBannerAddedInDetailsPage

--- src/components/HeaderFromDetails/HeaderFromDetails.js
+++ src/components/HeaderFromDetails/HeaderFromDetails.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./HeaderFromDetails.css";
 
 const HeaderFromDetails = (props) => {
-  const { headerImageLogo } = props;
+  const { headerImageLogo, headerBanner } = props;
 
   return (
-    <div className="header-from-details bg-image">
+    <div
+      style={{ backgroundImage: `url(${headerBanner})` }}
+      className="header-from-details bg-image"
+    >
       <div className="image-header-details">
         <img className="img-fluid pt-4" src={headerImageLogo} alt="" />
       </div>
--- src/components/LeagueDetails/LeagueDetails.js
+++ src/components/LeagueDetails/LeagueDetails.js
@@ -15,10 +15,13 @@ const LeagueDetails = () => {
       .then((data) => setTeamDetails(data.leagues[0]));
   }, [leagueKey]);
 
+  console.log(teamDetails);
+
   return (
     <div>
       <HeaderFromDetails
         headerImageLogo={teamDetails.strBadge}
+        headerBanner={teamDetails.strBanner}
       ></HeaderFromDetails>
       <LeagueDetailsContainer
         teamDetails={teamDetails}
